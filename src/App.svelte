e29211e697bba308608645394b245806bb5385b0
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-04-30 08:37:17 +0530

    basic 15puzzle with animated transitions 🎉

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,87 @@
 <script>
-	export let name;
+  import { flip } from "svelte/animate";
+
+  // no fancy puzzle generation yet
+  let puzzle = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, -1];
+  const empty = -1;
+  let emptyCellIndex = puzzle.indexOf(empty);
+  const rows = 4;
+
+  const canMove = (index, emptyCellIndex) => {
+    return (
+      Math.abs(index - emptyCellIndex) === 1 ||
+      Math.abs(index - emptyCellIndex) === rows
+    );
+  };
+
+  const handleMove = indexToMove => {
+    if (!canMove(indexToMove, emptyCellIndex)) {
+      return;
+    }
+    const numberToMove = puzzle[indexToMove];
+    puzzle = puzzle.map((number, index) => {
+      if (index === indexToMove) {
+        return -1;
+      } else if (number === -1) {
+        return numberToMove;
+      }
+      return number;
+    });
+    emptyCellIndex = puzzle.indexOf(empty);
+  };
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .container {
+    margin: 2rem auto;
+    display: flex;
+    justify-content: center;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .puzzle {
+    display: flex;
+    flex-wrap: wrap;
+    border: 5px solid purple;
+    justify-items: center;
+    width: 416px;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .cell,
+  .empty-cell {
+    display: inline-flex;
+    width: 100px;
+    height: 100px;
+    background-color: cadetblue;
+    border: 2px solid white;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    color: white;
+    cursor: pointer;
+  }
+
+  .empty-cell {
+    background-color: transparent;
+    border: 2px solid transparent;
+    pointer-events: none;
+  }
+
+  .no-move {
+    cursor: not-allowed;
+  }
 </style>
+
+<main class="container">
+  <div class="puzzle">
+    {#each puzzle as cellValue, index (cellValue)}
+      <div
+        animate:flip={{ duration: 300 }}
+        class:cell={index !== emptyCellIndex}
+        class:empty-cell={index === emptyCellIndex}
+        class:no-move={!canMove(index, emptyCellIndex)}
+        on:click={() => handleMove(index)}>
+        {cellValue}
+      </div>
+    {/each}
+  </div>
+</main>
