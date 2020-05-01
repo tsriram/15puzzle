07dfa1b24841f595efeafd0f4b95f40b559b10e6
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-05-01 13:49:08 +0530

    reset empty cell index after shuffle

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import GitHubCorner from "./GitHubCorner.svelte";
-  import shuffle from "lodash.shuffle";
   import { flip } from "svelte/animate";
+  import shuffle from "lodash.shuffle";
 
   // no fancy puzzle generation yet
   let puzzle = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, -1];
@@ -13,6 +13,7 @@
   function shufflePuzzle() {
     puzzle = shuffle(puzzle);
     moves = 0;
+    emptyCellIndex = puzzle.indexOf(empty);
   }
 
   const canMove = (index, emptyCellIndex) => {
