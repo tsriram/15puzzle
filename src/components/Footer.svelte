4b9146e251c27b5e9e34ae8392b7a812e3dd2610
Konstantin Nosov <nosov@nodeart.io>
2022-12-27 13:53:19 +0100

    remove copyright (it sucks but better not to cause problems to original developer)

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -16,17 +16,5 @@
 </style>
 
 <footer>
-  <div>
-    Built by
-    <a target="_blank" rel="noopener" href="https://twitter.com/tsriram">
-      Sriram
-    </a>
-    <span>|</span>
-    <a
-      target="_blank"
-      rel="noopener"
-      href="https://www.buymeacoffee.com/tsriram">
-      Buy me a coffee?
-    </a>
-  </div>
+
 </footer>
