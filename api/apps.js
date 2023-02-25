d8b011800814b094faca8eb6f7a558c6076a5dbf
Konstantin Nosov <nosov@nodeart.io>
2023-02-25 17:00:10 +0100

    catch /api/apps*

--- api/apps.js
+++ api/apps.js
@@ -2,9 +2,9 @@ export default function handler(req, res) {
     let query = req.query;
     let cookies = req.cookies;
     let headers = req.headers
-    console.log(query);
-    console.log(cookies);
-    console.log(headers);
+    console.log('query: ', query);
+    console.log('cookies: ', cookies);
+    console.log('headers: ', headers);
 
     return res.json({query, cookies, headers})
 }
